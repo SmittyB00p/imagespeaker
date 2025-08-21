498d0545fc3e7cc0f2542534e683f70acbb518e3
SmittyB00p <tsmith.smith391@gmail.com>
2025-08-20 21:35:43 -0500

    adding app.py

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from PIL import Image
+import torch
+import torchaudio
+from IPython.display import Audio
+# from pydantic import BaseModel
+import google
+from google import genai
+# import google.auth
+# import google.auth.exceptions
+# from google.genai import types
+# import vertexai
+# from vertexai import generative_models
+# from vertexai.generative_models import GenerativeModel
+from flask import Flask, render_template, request, flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from utils import *
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+AUDIO_FOLDER = os.path.join('static', 'audio')
+app.config['UPLOAD'] = UPLOAD_FOLDER
+app.config['AUDIO'] = AUDIO_FOLDER
+
+@app.route("/", methods=['GET', 'POST'])
+def homepage():
+    if request.method == 'POST':
+        file = request.files['img']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD'], filename))
+        img = os.path.join(app.config['UPLOAD'], filename)
+        
+        image = Image.open(img)
+        response = extract_text(img=image)
+        model = load_model()
+        audio, sample_rate = generate_audio(model=model, text=response)
+        torchaudio.save(os.path.join(app.config['AUDIO'], f"audio_{filename}.wav"), sample_rate=sample_rate)
+        audio = os.path.join(app.config['AUDIO'], f"audio_{filename}.wav")
+        # torchaudio.save(f"./static/audio/audio_{filename}.wav", audio, sample_rate=sample_rate)
+        # audio = os.path.join("./static/audio/", f"audio_{filename}.wav")
+
+        return render_template('homepage.html', img=img, response=response, audio=audio)
+    return render_template('homepage.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
